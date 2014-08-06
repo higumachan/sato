cc236139c8a3aa218bdf786115ce6250ec4c9a65
Yuta Hinokuma <harekumo792154@gmail.com>
2014-08-06 19:37:18 +0900

    test

--- model.cc
+++ model.cc
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <vector>
+#include <map>
 #include <random>
 
 using namespace std;
 
 const int AGENT_COUNT = 10;
-const int COMMODITY = 50;
+const int COMMODITY = 60;
 const int MAX_QUANTITY = 10;
-const int START_SALARY_STANDERD = 40;
+const int START_SALARY_STANDERD = 60;
 const int PAYMENT_AMOUNT = 20;
 const int RETIREMENT_AGE = 40;
 
 const int POOR = 1;
-const int COMMON = 3;
+const int COMMON = 5;
 const int RICH = 1;
 
-std::mt19937 engine;
+const double T = 1000.0;
+const double IMAGE_T = 15.0;
+
+mt19937 engine;
 
 struct HistoryData
 {
@@ -23,6 +27,10 @@ struct HistoryData
 	int target_stock;
 	int conduct;
 };
+
+vector<int> die_ages;
+int payment_count;
+map<int, int> payment_age_histgram;
 
 class Agent 
 {
@@ -65,7 +73,7 @@ public:
 		double hierarchy[3] = {POOR, COMMON, RICH};
 		discrete_distribution<int> dist(hierarchy, hierarchy + 3);
 		//start_salary = (dist(engine) + 1) * START_SALARY_STANDERD;
-		start_salary = pow(2, (dist(engine) - 1)) * START_SALARY_STANDERD;
+		start_salary = pow(4/3, (dist(engine) - 1)) * START_SALARY_STANDERD;
 	}
 	int update()
 	{
@@ -94,6 +102,7 @@ public:
 	{
 		int learn_amount = age - RETIREMENT_AGE;
 
+		die_ages.push_back(age);
 		//image[i] = 1.0;
 		printf("%d is die\n", id);
 		for (int i = 0; i < history.size(); i++){
@@ -101,14 +110,22 @@ public:
 			learn[hd.my_stock][hd.target_stock][hd.conduct] += learn_amount;
 		}
 		history = vector<HistoryData>();
+		for (int i = 0; i < AGENT_COUNT; i++){
+			image[i] = 0;
+		}
+		image[id] = -100000;
 		age = 0;
-		stock = 0;
+		stock = COMMODITY * 2;
 		set_hierarchy();
 	}
 
 	int decision_conduct(int my_stock, int target_stock)
 	{
-		discrete_distribution<int> dist(learn[my_stock][target_stock], learn[my_stock][target_stock] + 2);
+		double exp_learn[2];
+		double avg = (learn[my_stock][target_stock][0] + learn[my_stock][target_stock][1]) / 2;
+		exp_learn[0] = exp(learn[my_stock][target_stock][0] / avg);
+		exp_learn[1] = exp(learn[my_stock][target_stock][1] / avg);
+		discrete_distribution<int> dist(exp_learn, exp_learn + 2);
 		printf("%lf %lf\n", learn[my_stock][target_stock][0], learn[my_stock][target_stock][1]);
 		return dist(engine);
 	}
@@ -186,7 +203,11 @@ Agent agents[AGENT_COUNT];
 
 int Agent::payment()
 {
-	discrete_distribution<int> dist(image, image + AGENT_COUNT);
+	double exp_image[AGENT_COUNT];
+	for (int i = 0; i < AGENT_COUNT; i++){
+		exp_image[i] = exp(image[i] / IMAGE_T);
+	}
+	discrete_distribution<int> dist(exp_image, exp_image + AGENT_COUNT);
 	puts("image:");
 	for (int i = 0; i < AGENT_COUNT; i++){
 		printf("%lf ", image[i]);
@@ -213,6 +234,8 @@ int Agent::payment()
 			//Agents::down_image(id, 1);
 		}
 		else {
+			payment_count++;
+			payment_age_histgram[age]++;
 			target->add_stock(PAYMENT_AMOUNT);
 			stock -= PAYMENT_AMOUNT;
 			data.conduct = 1;
@@ -232,33 +255,45 @@ int Agent::payment()
 
 int main(void)
 {
-	engine = std::mt19937(10);
+	FILE* fp;
+	engine = std::mt19937(0.1);
 	int turn;
+
+	fp = fopen("age.csv", "w");
 
 	for (int i = 0; i < AGENT_COUNT; i++){
 		agents[i].init(i);
 	}
 	turn = 1;
-	while (1){
+	for (int i = 0; i < 1000000; i++){
 		puts("");
 		puts("");
 		puts("");
 		printf("trun%d\n", turn);
 		if (turn % 1000 == 0){
 			int sum_age = 0;
-			for (int i = 0; i < AGENT_COUNT; i++){
-				printf("%d\n", agents[i].get_age());
-				sum_age += agents[i].get_age();
+			for (int i = 0; i < die_ages.size(); i++){
+				//printf("%d\n", die_ages[i]);
+				sum_age += die_ages[i];
 			}
-			printf("ageavg=%lf\n", sum_age / double(AGENT_COUNT));
+			printf("%d, %d, ageavg=%lf\n",sum_age, die_ages.size(),  sum_age / (double)(die_ages.size()));
+			fprintf(fp, "%lf,%d,", sum_age / (double)(die_ages.size()), payment_count);
+			die_ages = vector<int>();
+			for (int i = 0; i < 100; i++){
+				fprintf(fp, "%d,", payment_age_histgram[i]);
+			}
+			fprintf(fp, "\n");
+			payment_age_histgram = map<int, int>();
+			payment_count = 0;
 			int n;
-			scanf("%d", &n);
+			//scanf("%d", &n);
 		}
 		for (int i = 0; i < AGENT_COUNT; i++){
 			agents[i].update();
 		}
 		turn++;
 	}
+	fclose(fp);
 	return (0);
 }
 
